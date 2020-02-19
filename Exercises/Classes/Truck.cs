06af384cf909815638f800dfa903b546254ec562
unknown <fortunato.mugnano88@gmail.com>
2020-02-19 14:03:14 -0600

    done interfaces

---
+++ Exercises/Classes/Truck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Classes
+{
+    class Truck : IAutomobile
+    {
+        public string LicensePlate
+        { get; }
+
+        public double Speed
+        { get; private set; }
+
+        public int Wheels
+        { get; }
+
+        public double Weight
+        { get; }
+
+        public Truck(double speed, double weight)
+        {
+            Speed = speed;
+            LicensePlate = "Random";
+            Weight = weight;
+
+            if (weight < 400)
+            {
+                Wheels = 8;
+            }
+            else
+            {
+                Wheels = 12;
+            }
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("HONK!");
+        }
+
+        public void SpeedUp()
+        {
+            Speed += 5;
+        }
+
+        public void SlowDown()
+        {
+            Speed -= 5;
+        }
+
+    }
+}
---
+++ Exercises/Interfaces/IAutomobile.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercises.Classes
+{
+    interface IAutomobile
+    {
+        string LicensePlate { get; }
+        double Speed { get; }
+        int Wheels { get; }
+        void Honk();
+    }
+}
